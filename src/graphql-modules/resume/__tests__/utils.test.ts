1e933eee50432163c9e7bd1bab2e1a576d155371
aretecode <aretecode@gmail.com>
2019-04-18 17:22:30 -0700

    isValidResponse - split to validate obj + indexed

--- src/graphql-modules/resume/__tests__/utils.test.ts
+++ src/graphql-modules/resume/__tests__/utils.test.ts
@@ -1,34 +1,37 @@
+import 'jest'
 import { isValidResponse } from '../utils'
 
 /**
  * we may want to validate without top level key, or check 2 types
  */
-const MOCK_VALID_RESPONSE = {
-  resume: {
-    basics: {
-      name: '',
-      label: '',
-      picture: '',
-      email: '',
-      telephone: '',
-      website: '',
-      summary: '',
-      profiles: [],
-      address: '',
-      postalCode: '',
-      city: '',
-      countryCode: '',
-      region: '',
-      resumeWebsite: '',
-      skills: [],
-    },
-    work: [],
+const MOCK_RESUME_OBJ = {
+  basics: {
+    name: '',
+    label: '',
+    picture: '',
+    email: '',
+    telephone: '',
+    website: '',
+    summary: '',
+    profiles: [],
+    address: '',
+    postalCode: '',
+    city: '',
+    countryCode: '',
+    region: '',
+    resumeWebsite: '',
+    skills: [],
   },
+  work: [],
+}
+const MOCK_VALID_RESPONSE = {
+  resume: MOCK_RESUME_OBJ,
 }
 
 describe('resume/utils', () => {
   it('should identify a valid response', () => {
     expect(isValidResponse(MOCK_VALID_RESPONSE)).toBe(true)
+    expect(isValidResponse(MOCK_RESUME_OBJ)).toBe(true)
   })
   it('should not pass for false positives', () => {
     const LIST_OF_INVALID_RESPONSES = [
--- src/graphql-modules/resume/utils.ts
+++ src/graphql-modules/resume/utils.ts
@@ -7,14 +7,27 @@ export function isObj(x: any): x is { [key: string]: any } | any[] {
   return x !== null && typeof x === 'object'
 }
 
-export function isValidResponse(
-  response: { resume?: Partial<Resume> } | null | undefined
-): response is ResumeIndexedResponse {
+export function isValidResumeValue(resume: Partial<Resume>): resume is Resume {
   return (
-    isObj(response) === true &&
-    isObj(response.resume) === true &&
-    Array.isArray(response.resume.work) &&
-    isObj(response.resume.basics) === true &&
-    Object.keys(response.resume.basics).length > 0
+    isObj(resume) === true &&
+    Array.isArray(resume.work) &&
+    isObj(resume.basics) === true &&
+    Object.keys(resume.basics).length > 0
   )
+}
+
+/**
+ * @todo I am not sure where it was being indexed with `resume`
+ */
+export function isValidResponse(
+  response: { resume?: Partial<Resume> } | Resume | null | undefined
+): response is ResumeIndexedResponse | Resume {
+  if (isObj(response) === false) {
+    return false
+  } else {
+    return (
+      isValidResumeValue((response as ResumeIndexedResponse).resume) === true ||
+      isValidResumeValue(response as Resume) === true
+    )
+  }
 }
